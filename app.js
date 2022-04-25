705df0b5091a73a191318b404c8776d6e378ac79
eric <ericwang42@outlook.com>
2022-04-25 11:10:15 -0700

    update

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const reset = document.querySelector(".reset"); //user input reset button
 const list = document.querySelector(".userInputContent"); //content list div
 const div = document.querySelector(".contentDiv"); //content div
 const paragraph = document.querySelector(".contentParagraph"); //content paragraph
-const binDelete = document.querySelector(".binDelete"); //content svg delete
+const binDelete = document.querySelector(".binDelete");
 
 //appends user input value
 const appendList = function () {
   list.insertAdjacentHTML(
-    "afterbegin",
+    "beforebegin",
     `<div class="contentDiv"><p class="contentParagraph">${input.value}</p><button class="binDelete"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
     <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
   </svg></button></div>`
